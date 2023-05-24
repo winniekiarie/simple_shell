7a8722f62fc8482adb6227211d69b40e2432e98b
winniekiarie <kiariebsc@gmail.com>
2023-05-24 11:26:33 -0700

    main simple

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - main function
+ * @argc: number of arguements
+ * @argv: arguement
+ * @envp: program’s environment
+ * Return: 0 for sucess
+ */
+int main(int argc, char **argv, char **envp)
+{
+	if (argc == 1 || argv[1] == NULL)
+	loop(envp);
+
+	return (0);
+}
